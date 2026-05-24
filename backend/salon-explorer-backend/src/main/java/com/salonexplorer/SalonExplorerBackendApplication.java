bd0aeef70d7d06da858b680bc58e70d21a5821d6
MarkOmelyanenko <famil.mark@gmail.com>
2026-05-24 14:30:40 +0200

    Initialize full-stack project structure

---
+++ backend/salon-explorer-backend/src/main/java/com/salonexplorer/SalonExplorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salonexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonExplorerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonExplorerBackendApplication.class, args);
+    }
+
+}
---
+++ backend/salon-explorer-backend/src/test/java/com/salonexplorer/SalonExplorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salonexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonExplorerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
